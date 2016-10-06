c479e1ece642d110379f46f0d231a8b613072ec2
Dugong <js0628@ymail.com>
2016-10-06 22:21:09 +0900

    init

---
+++ UI_Practice/UI_Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI_Practice
+//
+//  Created by Joosung Kim on 06/10/2016.
+//  Copyright © 2016 Joosung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
